b2e956df0250b9019d58d5a96b2607252fb84413
minhaol <350687497@qq.com>
2019-04-08 12:49:21 +1000

    version1

---
+++ controller/router.js
@@ -0,0 +1,3 @@
+exports.getIndex=function(req,res){
+    res.render("index.ejs");
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const router=require("./controller/router.js");
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app=express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static("./public"));
+app.get("/",router.getIndex);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
