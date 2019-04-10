5377dcbb535977db4eb600c4a0ae862774475123
minhaol <350687497@qq.com>
2019-04-10 14:56:06 +1000

    version1.1

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require('express');
 const router=require("./controller/router.js");
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 var app=express();
+
+var a=[{"name":"name1","age":1},{"name":"name2","age":2},{"name":"name3","age":3}];
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static("./public"));
+
+
 app.get("/",router.getIndex);
+
+app.get("/findstudent",function(req,res){
+    res.json(a);  
+});
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
